4b15b76b08b207c4769cc7919505296f3b9fba56
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-06-06 21:45:26 +0000

    Display images if image url is in message

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 
-const Message = ({ type, content, username }) =>
+const getImages = (message) =>
 {
+  if (message.match(/(\S)+\.(jpg|jpeg|png|gif)/g))
+  {
+    const images = message.match(/(\S)+\.(jpg|jpeg|png|gif)/g);
+    return images;
+  }
+  else return false;
+}
+
+const Message = ({ type, content, username, color }) =>
+{
+  const images = getImages(content);
+  const imageElements = (images && images.map( (image) =>
+    {
+      console.log("Here");
+      return (<img src={image} />);
+    }));
   return (
     <div>
-      { type === 'incomingMessage' ?
+      { type === 'incomingMessage' &&
         (
-          <div className="message">
-            <span className="message-username">{ username }</span>
-            <span className="message-content">{ content }</span>
+          <div>
+            <div className="message">
+              <span className="message-username" style={{color}}>{ username }</span>
+              <span className="message-content">{ content }</span>
+            </div>
+          { imageElements }
           </div>
-        ) :
+        ) }
+      { type === 'incomingNotification' &&
         (
           <div className="message system">
             { content }
           </div>
-        )}
+        ) }
     </div>
   );
 }
