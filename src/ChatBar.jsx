3bad5eb44ac9c2a5c51830c3abdc08169e88e9f0
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-06-06 21:44:44 +0000

    Disable message input if name is changed but not submitted

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
-const ChatBar = ({currentUser, submitMessage, submitUsername}) =>
+const ChatBar = ({currentUser, submitMessage, submitUsername, usernameSubmitted}) =>
 {
   return (
     <form className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={currentUser} onKeyPress={submitUsername}/>
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={submitMessage}/>
+      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={currentUser} onKeyUp={submitUsername} />
+      <input className="chatbar-message" placeholder={(usernameSubmitted) ? "Type a message and hit ENTER" : "Please submit your new username"} onKeyUp={submitMessage} disabled={(usernameSubmitted) ? "" : "disabled"} />
     </form>
   )
 }
